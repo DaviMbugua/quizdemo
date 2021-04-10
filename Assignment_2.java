045cc4ec646c21acc4fd4dc50c047db805a1c0d0
DaviMbugua <70985356+DaviMbugua@users.noreply.github.com>
2021-04-10 12:24:25 +0300

    task2

--- Assignment_2.java
+++ Assignment_2.java
@@ -1,198 +1,170 @@
 import java.util.Scanner; //importing scanner to allow user input
+
 public class Assignment_2 {
 
-	//public static void  main(String[] args)  {
-		//System.out.println("Welcome to the quiz game; press a");
-	//	start_quiz();
-	
-}
-	public static void start_quiz() {       //creating a new method 
+	public static void main(String[] args) {
+		System.out.println("Welcome to the quiz game!");
+
+		start_quiz();
+	}
+
+	public static void start_quiz() { // creating a new method
 		Scanner quiz = new Scanner(System.in);
-		String response = quiz.nextLine();
 		int numCorrect = 0;
-		int a = 0;
-		while(a<10) {               //do a while loop 
-			
+		int answered = 0;
+
+		while (true) { // do a while loop
+
 			System.out.println("Who is the founder of ALU?");
 			System.out.println("a. Fred ");
 			System.out.println("b. David");
-			a++;
-			
-			
-			if (response.equals("a"))
-			{
+			String response1 = quiz.nextLine();
+			answered++;
+
+			if (response1.equals("a")) {
 				System.out.println("Correct!");
-				numCorrect  += 1;
-			}
-			else
-			{
+				numCorrect += 1;
+			} else {
 				System.out.println("Wrong!");
 			}
-			
+
 			System.out.println("When was ALU founded?");
 			System.out.println("a. 2005 ");
 			System.out.println("b. 2017");
-			a++;
-			
-			response = quiz.nextLine();
-			if (response.equals("b"))
-			{
+			String response2 = quiz.nextLine();
+			answered++;
+
+			if (response2.equals("b")) {
 				System.out.println("Correct!");
 				numCorrect += 1;
-			}
-			else
-			{
+			} else {
 				System.out.println("Wrong!");
 			}
-			
+
 			System.out.println("Who teaches programming classes?");
 			System.out.println("a. Ella ");
 			System.out.println("b. Javan");
-			a++;
-			
-			response = quiz.nextLine();
-			if (response.equals("a"))
-			{
+			String response3 = quiz.nextLine();
+			answered++;
+
+			if (response3.equals("a")) {
 				System.out.println("Correct!");
 				numCorrect += 1;
-			}
-			else
-			{
+			} else {
 				System.out.println("Wrong!");
-			}	
-		    
+			}
+
 			System.out.println("Which among the 2 languages is hardest?");
 			System.out.println("a. Java ");
 			System.out.println("b. Python");
-			a++;
-			
-			response = quiz.nextLine();
-			if (response.equals("a"))
-			{
+			String response4 = quiz.nextLine();
+			answered++;
+
+			if (response4.equals("a")) {
 				System.out.println("Correct!");
 				numCorrect += 1;
-			}
-			else
-			{
+			} else {
 				System.out.println("Wrong!");
-			}	
-		    
+			}
+
 			System.out.println("How many branches does ALU have?");
 			System.out.println("a. 4 ");
 			System.out.println("b. 2");
-			a++;
-			
-			response = quiz.nextLine();
-			if (response.equals("b"))
-			{
+			String response5 = quiz.nextLine();
+			answered++;
+
+			if (response5.equals("b")) {
 				System.out.println("Correct!");
 				numCorrect += 1;
-			}
-			else
-			{
+			} else {
 				System.out.println("Wrong!");
 			}
-			
+
 			System.out.println("How many courses does ALU offer?");
 			System.out.println("a. 8 ");
 			System.out.println("b. 10");
-			a++;
-			
-			response = quiz.nextLine();
-			if (response.equals("a"))
-			{
+			String response6 = quiz.nextLine();
+			answered++;
+
+			if (response6.equals("a")) {
 				System.out.println("Correct!");
 				numCorrect += 1;
-			}
-			else
-			{
+			} else {
 				System.out.println("Wrong!");
 			}
-			
+
 			System.out.println("How many countries are in Africa?");
 			System.out.println("a. 54 ");
 			System.out.println("b. 49");
-			a++;
-			response = quiz.nextLine();
-			if (response.equals("a"))
-			{
+			String response7 = quiz.nextLine();
+			answered++;
+
+			if (response7.equals("a")) {
 				System.out.println("Correct!");
 				numCorrect += 1;
-			}
-			else
-			{
+			} else {
 				System.out.println("Wrong!");
 			}
 			System.out.println("What is the capital city of Rwanda?");
 			System.out.println("a. Nairobi ");
 			System.out.println("b. Kigali");
-			a++;
-			
-			response = quiz.nextLine();
-			if (response.equals("b"))
-			{
+			String response8 = quiz.nextLine();
+			answered++;
+
+
+			if (response8.equals("b")) {
 				System.out.println("Correct!");
 				numCorrect += 1;
-			}
-			else
-			{
+			} else {
 				System.out.println("Wrong!");
 			}
+
 			System.out.println("Who is ALU Rwanda's dean?");
 			System.out.println("a. Gaidi ");
 			System.out.println("b. Aseda");
-			a++;
-			
-			response = quiz.nextLine();
-			if (response.equals("a"))
-			{
+			String response9 = quiz.nextLine();
+			answered++;
+
+
+			if (response9.equals("a")) {
 				System.out.println("Correct!");
 				numCorrect += 1;
-			}
-			else
-			{
+			} else {
 				System.out.println("Wrong!");
 			}
-			
+
 			System.out.println("Who is in charge of student life at ALU Rwanda ?");
 			System.out.println("a. Bobson ");
 			System.out.println("b. Sila");
-			a++;
-			
-			response = quiz.nextLine();
-			if (response.equals("b"))
-			{
+			String response10 = quiz.nextLine();
+			answered++;
+
+			if (response10.equals("b")) {
 				System.out.println("Correct!");
 				numCorrect += 1;
-			}
-			else
-			{
+			} else {
 				System.out.println("Wrong!");
 			}
-			
+
+			System.out.println("You answered : " + numCorrect + " correctly and " + (10 - numCorrect) + " wrong");
+
 			System.out.println("Do you want to play the game again?");
-			System.out.println("1. YES");
-			System.out.println("0. NO");
-			
-			response = quiz.nextLine();
-			if (response.equals("0"))
-			{ 
-				start_quiz();
+			System.out.println("yes");
+			System.out.println("no");
+			String playAgain = quiz.nextLine();
+
+			if (playAgain.equals("yes")) {
+				answered = 0;
+				numCorrect = 0;
+				continue;
 			}
-		
-			else
-			{
-				a++;
-				
+			else {
+				System.out.println("You answered : " + numCorrect + " correctly and " + (10 - numCorrect) + " wrong");
+				break;
 			}
-		
-			
+
 		}
-		
-	    
-		
 
 	}
-  
 
 }
